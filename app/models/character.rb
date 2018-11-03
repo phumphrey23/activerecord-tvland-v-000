7ed728cbaa14f997001e8b316e634a2316f592fc
Precious Humphrey <phumphrey23@gmail.com>
2018-11-03 19:17:29 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,8 +2,8 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
 
-  def catchphrase
+  def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
-  
+
 end
